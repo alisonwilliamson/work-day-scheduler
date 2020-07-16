3cdd2f065abb05efa5e2aa75e74cebbe0d05ba06
Allie <williamson.alisonj@gmail.com>
2020-07-16 00:24:34 -0400

    deleted console.logs

--- JS/script.js
+++ JS/script.js
@@ -12,15 +12,11 @@ $(document).ready(function () {
         let hour = $(this).parent().attr("id");
         // saves the hour and description to local storage
         localStorage.setItem(hour, description);
-
-        console.log(hour)
-        console.log(description)
     });
 
     // gets the hour and description from local storage and displays it
     $(".description").each(function(){
         $(this).val(localStorage.getItem($(this).parent().attr("id")));
-  
     });
 
     function hourChecker() {
